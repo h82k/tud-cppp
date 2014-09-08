924345935be8b59b6e4a09b76539fdb7cfe07fe9
roland.kluge.de@googlemail.com <RolandKluge@users.noreply.github.com>
2014-09-08 11:31:25 +0000

    Move script for copying exercise sheets from TeX projects

--- copy_exercise_sheets_from_tex_sources.ps1
+++ copy_exercise_sheets_from_tex_sources.ps1
@@ -2,11 +2,14 @@
 # Copies the exercise sheets from the tex projects to this folder
 #
 
+$exercises="Übungen"
+
 $days= "tag1","tag2","tag3","tag4","tag5","tag6"
+
 
 ForEach($day in $days) {
     echo "Copying $day.pdf"
-    copy "./sources/$day/$day.pdf" .
+    copy "$exercises/sources/$day/$day.pdf" $exercises
 }
 
 
