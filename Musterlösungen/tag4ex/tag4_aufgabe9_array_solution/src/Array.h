952773e19cb49d203dc6187e383bdb08b2efc8b0
roland.kluge.de@googlemail.com <RolandKluge@users.noreply.github.com>
2014-09-12 12:53:55 +0000

    Add solution for 'Array<T>' exercise

---
+++ Musterlösungen/tag4ex/tag4_aufgabe9_array_solution/src/Array.h
@@ -0,0 +1,135 @@
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+#include <iostream>
+#include <boost/smart_ptr.hpp>
+
+typedef unsigned int size_t;
+
+/**
+ * Custom implementation of an array.
+ *
+ * This class encapsulates a raw array together with its size.
+ *
+ */
+template<class T>
+class Array {
+	typedef boost::shared_ptr<T> TPtr;
+public:
+	/**
+	 * Creates an array that can hold 'size'
+	 * elements of its contained type.
+	 */
+	Array(size_t size);
+	~Array();
+
+	/**
+	 * Returns the element of the array at the
+	 * given index.
+	 */
+	T& operator[](size_t index);
+	const T& operator[](size_t index) const;
+
+	/**
+	 * Returns an array that starts 'offset'
+	 * items behind the current array.
+	 */
+	Array<T> operator+(size_t offset);
+
+	/**
+	 * Returns the size of this array.
+	 */
+	size_t getSize() const;
+
+private:
+	/**
+	 * Internal constructor that produces a 'view' of the array
+	 * that is shifted by 'offset'.
+	 *
+	 * Example:
+	 * Given an array of size 10 and an offset of 2, the created array
+	 * will have a size of 8 and its first element will be the third
+	 * element of 'other'.
+	 */
+	Array(const Array<T> &other, size_t offset);
+
+	/**
+	 * The size of the underlying array.
+	 * The true size is size - offset.
+	 */
+	size_t size;
+
+	/**
+	 * The underlying array, may be shared by multiple arrays that
+	 * represent shifted views of the same underlying data.
+	 */
+	TPtr data;
+
+	/**
+	 * The offset in the underlying array.
+	 */
+	size_t offset;
+};
+
+template<class T>
+std::ostream &operator<<(std::ostream &stream, const Array<T> &array);
+
+#include <stdexcept>
+#include <cstdlib>
+#include <sstream>
+
+template<class T>
+Array<T>::Array(size_t size) :
+		size(size), offset(0) {
+	data = TPtr(new T[size]);
+}
+
+template<class T>
+Array<T>::Array(const Array<T> &other, size_t offset) :
+		size(other.size), data(other.data), offset(offset) {
+}
+
+template<class T>
+Array<T>::~Array() {
+}
+
+template<class T>
+size_t Array<T>::getSize() const {
+	return size - offset;
+}
+
+template<class T>
+T &Array<T>::operator [](size_t index) {
+	if (index >= getSize()) {
+		std::stringstream message;
+		message << "Index out of bounds. Index: " << index << ", size: "
+				<< getSize() << ".";
+		throw std::out_of_range(message.str());
+	}
+	return (data.get())[index + offset];
+}
+
+template<class T>
+const T &Array<T>::operator [](size_t index) const {
+	if (index >= getSize()) {
+		std::stringstream message;
+		message << "Index out of bounds. Index: " << index << ", size: "
+				<< getSize() << ".";
+		throw std::out_of_range(message.str());
+	}
+	return (data.get())[index + offset];
+}
+
+template<class T>
+Array<T> Array<T>::operator+(size_t delta) {
+	if (delta >= getSize()) {
+		std::stringstream message;
+		message << "Offset larger than array size. Offset: " << delta << ", size: "
+				<< getSize() << ".";
+		throw std::out_of_range(message.str());
+	}
+	Array<T> offsetArray(*this, offset + delta);
+	return offsetArray;
+}
+
+#endif /* ARRAY_H_ */
---
+++ Musterlösungen/tag4ex/tag4_aufgabe9_array_solution/src/main.cpp
@@ -0,0 +1,52 @@
+#include "Array.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<class T>
+void printNthElement(const Array<T> &array, size_t position) {
+	cout << array[position] << endl;
+}
+
+int main(int argc, char **argv) {
+	Array<std::string> stringArray(10);
+	stringArray[0] = "First entry";
+	stringArray[1] = "Second entry";
+	stringArray[9] = "Last entry";
+
+	printNthElement(stringArray, 0);
+	printNthElement(stringArray, 9);
+	try {
+		printNthElement(stringArray, 10);
+	} catch (out_of_range &ex) {
+		cout << "Oh oh: " << ex.what() << endl;
+	}
+
+	/*
+	 * Create a view of the array
+	 */
+	Array<std::string> arrayFromOffset1 = stringArray + 1;
+	printNthElement(arrayFromOffset1, 0);
+
+	try {
+		printNthElement(arrayFromOffset1, 9);
+	} catch (out_of_range &ex) {
+		cout << "Oh oh: " << ex.what() << endl;
+	}
+
+	try {
+		// Create an invalid view (offset too large) of the orignal array.
+		Array<string> arrayFromOffset2 = stringArray + 10;
+	} catch (out_of_range &ex) {
+		cout << "Oh oh: " << ex.what() << endl;
+	}
+
+	// Check that accumulating offset (applying operator+ twice) works
+	try {
+		Array<string> arrayFromOffset3 = arrayFromOffset1 + 9;
+	} catch (out_of_range &ex) {
+		cout << "Oh oh: " << ex.what() << endl;
+	}
+	return 0;
+}
