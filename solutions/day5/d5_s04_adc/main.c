0a8153a5c58fd9fcf85a4dbcd30c47142481a5b5
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-07-30 16:11:17 +0200

    improved exercise 4 of day 5 (adc)

--- solutions/day5/d5_s04_adc/main.c
+++ solutions/day5/d5_s04_adc/main.c
@@ -1,7 +1,6 @@
 #include "mb96348hs.h"
 
 const char DEC7SEG[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
-#define DELAY 300000l
 // definitions to increase readability
 #define SEG_LEFT PDR09
 #define SEG_RIGHT PDR00
@@ -44,53 +43,53 @@ int scaleInt(int num) {
 	return (num * 10) / 256;
 }
 
-//returns the value for specified channel
-int getADValue(int channel) {
+// return ADC value of the specified channel (either 1 or 2)
+int getADCValue(int channel) {
 	int result;
-	
-	ADSR = 0x6C00 + (channel << 5) + channel; 			  // Start and end channel is channelNumber
-	ADCS_STRT = 1;										  // Start A/D conversion
-	while(ADCS_INT == 0) {} 							  // Wait for conversion to finish
-	result = ADCRL;										  // Store result (1 Byte)
-	ADCRL = 0;										      // Set bit to 0 for next conversion
+	ADSR = 0x6C00 + (channel << 5) + channel;	// start and end channel is channelNumber
+	ADCS_STRT = 1;								// start A/D conversion
+	while(ADCS_INT == 0) {} 					// wait for conversion to finish
+	result = ADCRL;								// store result (1 byte)
+	ADCRL = 0;									// set bit to 0 for next conversion
 	return result;
 }
 
-void main(void) {
-	/* definitions go here */
-	int value;
-
-	// initialize I/O-ports
-	PDR00  = 0xff;
-	DDR00  = 0xff;		// Set Port00 as output (right 7Segment display)
-	PDR09  = 0xff;
-	DDR09  = 0xff;		// Set Port09 as output (left 7Segment display)
-
-	PDR07  = 0x00;
-	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons)
-							// 0xfc = 11111100 (bin)
-	PIER07 = 0x03;		// enable input
-							// 0x03 = 00000011 (bin)
-
-	// Initialisation of AD converter
+// init A/D converter
+void initADC(void) {
 	ADCS_MD = 3; 		// ADC Stop Mode
 	ADCS_S10 = 1;		// 8 Bit Precision
 	ADER0_ADE1 = 1;		// Activate analog input AN1 + AN2
 	ADER0_ADE2 = 1;		// (ADER0: Inputs AN0 - AN7)
+}
 
+// init I/O-ports
+void initIO(void) {
+	PDR00  = 0xff;
+	DDR00  = 0xff;		// set port00 as output (right seven-segment display)
+	PDR09  = 0xff;
+	DDR09  = 0xff;		// set port09 as output (left seven-segment display)
+	PDR07  = 0x00;
+	DDR07  = 0xfc;		// set P07_0 and P07_1 as input (buttons) - 0xfc = 11111100 (bin)
+	PIER07 = 0x03;		// enable input - 0x03 = 00000011 (bin)
+}
+
+void main(void) {
+	// definitions
+	int value;
+	
+	// init buttons and seven-segment displays
+	initIO();
+	// init A/D converter
+	initADC();
+	
 	for (;;) {
-		// read the AD value from channel 1 and display its value
-		value = getADValue(1);
+		// read the value from channel 1 and display the value
+		value = getADCValue(1);
 		setLeft7Seg(scaleInt(value));
-		
 		wait(5);
-		
-		// read the AD value from channel 2 and display its value
-		value = getADValue(2);
+		// read the value from channel 2 and display the value
+		value = getADCValue(2);
 		setRight7Seg(scaleInt(value));
-		
 		wait(5);
-
 	}
-
 }
