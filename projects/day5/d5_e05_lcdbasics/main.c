5db4923c808d940758036f72e34cb4cc64cd275d
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-07-30 16:17:26 +0200

    improved code of ex5 of day 5 (lcd basics)

--- projects/day5/d5_e05_lcdbasics/main.c
+++ projects/day5/d5_e05_lcdbasics/main.c
@@ -1,7 +1,7 @@
 #include "mb96348hs.h"
 
 const char DEC7SEG[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
-
+// definitions to increase readability
 #define LCD_PORT_DB     PDR01
 #define LCD_PIN_DI      PDR02_P0
 #define LCD_PIN_RW      PDR02_P1
@@ -9,48 +9,43 @@ const char DEC7SEG[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 
 #define LCD_PIN_CS1     PDR02_P3
 #define LCD_PIN_CS2     PDR02_P4
 #define LCD_PIN_RESET   PDR02_P5
-
+// lcd wait time
 #define LCD_T 10l
-
-
 
 /* your functions here */
 
-
-
-void main(void) {
-	/* definitions go here */
-	
-	
-	
-	// initialize I/O-ports
+// init I/O-ports
+void initIO(void) {
 	PDR00  = 0xff;
-	DDR00  = 0xff;		// Set Port00 as output (right 7Segment display)
+	DDR00  = 0xff;		// set port00 as output (right seven-segment display)
 	PDR09  = 0xff;
-	DDR09  = 0xff;		// Set Port09 as output (left 7Segment display)
-	
+	DDR09  = 0xff;		// set port09 as output (left seven-segment display)
 	PDR07  = 0x00;
-	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons)
-							// 0xfc = 11111100 (bin)
-	PIER07 = 0x03;		// enable input
-							// 0x03 = 00000011 (bin)
-	
-	// initialize LCD
+	DDR07  = 0xfc;		// set P07_0 and P07_1 as input (buttons) - 0xfc = 11111100 (bin)
+	PIER07 = 0x03;		// enable input - 0x03 = 00000011 (bin)
+}
+
+// init LCD
+void initLCD(void) {
 	PDR01 = 0x00;
 	DDR01 = 0xff;
 	PDR02 = 0x00;
 	DDR02 = 0xff;
 	LCD_PIN_RESET = 1;	// RESET must be OFF (1)
-	LCD_PIN_RW    = 0;	// write always
+	LCD_PIN_RW = 0;		// write always
+}
+
+void main(void) {
+	/* your definitions here */
 	
-	
+	// init buttons and seven-segment displays
+	initIO();
+	// init LCD
+	initLCD();
 	
 	/* your code here */
-	
-	
 	
 	for (;;) {
 		__wait_nop();
 	}
-	
 }
