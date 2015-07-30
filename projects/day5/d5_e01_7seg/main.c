fff0c66f5e2664c0f53092f8723a560ec204543f
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-07-30 15:22:31 +0200

    improved code readability of exercise 1 of day 5

--- projects/day5/d5_e01_7seg/main.c
+++ projects/day5/d5_e01_7seg/main.c
@@ -3,33 +3,26 @@
 const char DEC7SEG[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 #define DELAY 1000000l
 
-
-
-void main(void) {
-	/* your definitions here */
-	
-	
-	
-	// initialize I/O-ports
+// initialize I/O-ports
+void initIO(void) {
 	PDR00  = 0xff;
 	DDR00  = 0xff;		// Set Port00 as output (right 7Segment display)
 	PDR09  = 0xff;
 	DDR09  = 0xff;		// Set Port09 as output (left 7Segment display)
-	
 	PDR07  = 0x00;
-	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons)
-							// 0xfc = 11111100 (bin)
-	PIER07 = 0x03;		// enable input
-							// 0x03 = 00000011 (bin)
+	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons) - 0xfc = 11111100 (bin)
+	PIER07 = 0x03;		// enable input - 0x03 = 00000011 (bin)
+}
+
+void main(void) {
+	/* your definitions here */
 	
-	
+	// initialisations
+	initIO();
 	
 	/* your code here */
-	
-	
 	
 	for (;;) {
 		__wait_nop();
 	}
-	
 }
--- solutions/day5/d5_s01_7seg/main.c
+++ solutions/day5/d5_s01_7seg/main.c
@@ -3,23 +3,25 @@
 const char DEC7SEG[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 #define DELAY 1000000l
 
-void main(void) {
-	/* your definitions here */
-	long i, j, d;
-	
-	// initialize I/O-ports
+// initialize I/O-ports
+void initIO(void) {
 	PDR00  = 0xff;
 	DDR00  = 0xff;		// Set Port00 as output (right 7Segment display)
 	PDR09  = 0xff;
 	DDR09  = 0xff;		// Set Port09 as output (left 7Segment display)
-	
 	PDR07  = 0x00;
-	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons)
-							// 0xfc = 11111100 (bin)
-	PIER07 = 0x03;		// enable input
-							// 0x03 = 00000011 (bin)
-							
-	/* your code here */
+	DDR07  = 0xfc;		// Set P07_0 and P07_1 as input (buttons) - 0xfc = 11111100 (bin)
+	PIER07 = 0x03;		// enable input - 0x03 = 00000011 (bin)
+}
+
+void main(void) {
+	// definitions
+	long i, j, d;
+	
+	// initialisations
+	initIO();
+	
+	// code
 	for (i = 0; i < 10; ++i) {
 		for (j = 0; j < 10; ++j) {
 			// display digits i and j
@@ -35,5 +37,4 @@ void main(void) {
 	for (;;) {
 		__wait_nop();
 	}
-	
 }
