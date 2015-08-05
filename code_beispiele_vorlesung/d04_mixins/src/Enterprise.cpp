fa8aa97db87833a0662b479f8fff9b860a1f15a1
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-08-05 09:05:19 +0200

    Renaming

---
+++ code_beispiele_vorlesung/d04_mixins/src/Enterprise.cpp
@@ -0,0 +1,18 @@
+/*
+ * Enterprise.cpp
+ *
+ *  Created on: 02.08.2013
+ *      Author: anjorin
+ */
+
+#include "Enterprise.h"
+
+Enterprise::Enterprise() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Enterprise::~Enterprise() {
+	// TODO Auto-generated destructor stub
+}
+
