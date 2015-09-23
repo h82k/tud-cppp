7fe0eb8f3c010f7b3355d0c609e6ab06e5231640
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-09-23 18:10:09 +0200

    improved syntax and added author to pong game

--- solutions/day6/d6_pong_mg_2012/main.c
+++ solutions/day6/d6_pong_mg_2012/main.c
@@ -1,4 +1,16 @@
+/**
+ * Author: Matthias Gazzari
+ * Created: 20.09.2012
+ * Revised: 23.09.2014
+ */
+
 #include "mb96348hs.h"
+#include "16FXlib/util.h"
+#include "16FXlib/lcd.h"
+#include "16FXlib/adc.h"
+#include "16FXlib/seg.h"
+#include "16FXlib/buttons.h"
+#include "math.h"
 
 #define GAME_WIDTH 128
 #define GAME_HEIGHT 64
@@ -11,13 +23,6 @@
 #define CONVERT (GAME_HEIGHT - BAR_HEIGHT) / (ADC_COUNT-1)
 #define LEFT_COLLISION GAME_WIDTH - 1 - BAR_WIDTH
 #define RIGHT_COLLISION 0 + BAR_WIDTH
-
-#include "16FXlib/util.h"
-#include "16FXlib/lcd.h"
-#include "16FXlib/adc.h"
-#include "16FXlib/seg.h"
-#include "16FXlib/buttons.h"
-#include "math.h"
 
 typedef struct ballData {
 	float xPos;
@@ -35,30 +40,15 @@ typedef struct playerData {
 } playerData;
 
 void ball_init(ballData* ball) {
-	// TODO make randomOne/Two better!!!
+	// TODO improve initial ball direction
 	float randomOne = random() % 1000 - 500 + 1;
 	float randomTwo = (random() % 4 - 2) * randomOne;
 	float factor = sqrt(randomOne*randomOne + randomTwo*randomTwo);
 	randomOne /= factor;
 	randomTwo /= factor;
-	seg_num(randomOne);
 	// BALL_SPEED = px/cycle
 	// GAME_MS_PER_FRAME = ms/cycle
 	// BALL_SPEED/GAME_MS_PER_FRAME = px/ms
-	ball->xPos = GAME_WIDTH/2;
-	ball->yPos = GAME_HEIGHT/2;
-	ball->speed = BALL_SPEED / GAME_MS_PER_FRAME;
-	ball->xDir = randomTwo;		//-4;
-	ball->yDir = randomOne; 	//3
-}
-
-void restart(ballData* ball) {
-	// TODO make randomOne/Two better!!!
-	float randomOne = random() % 1000 - 500 + 1;
-	float randomTwo = (random() % 4 - 2) * randomOne;
-	float factor = sqrt(randomOne*randomOne + randomTwo*randomTwo);
-	randomOne /= factor;
-	randomTwo /= factor;
 	ball->xPos = GAME_WIDTH/2;
 	ball->yPos = GAME_HEIGHT/2;
 	ball->speed = BALL_SPEED / GAME_MS_PER_FRAME;
@@ -73,39 +63,26 @@ void player_init(playerData* player) {
 	player->pointsRight = 0;
 }
 
-void ball_reflect(ballData* ball, int moveX) {
-	if (moveX)
+void ball_reflect(ballData* ball, int horizontal) {
+	if (horizontal) {
 		ball->xDir = - ball->xDir;
-	else
+	}
+	else {
 		ball->yDir = - ball->yDir;
-}
-
-/*
-void check_collision(ballData* ball) {
-	if (ball->yPos == 0 || ball->yPos == GAME_HEIGHT - 1){
-		ball_reflect(ball, 0);
-	}
-	if (ball->xPos == 0 || ball->xPos == GAME_WIDTH - 1){
-		ball_reflect(ball, 1);
 	}
 }
-*/
 
 int in_range(int ballPos, int barPos) {
-	if (ballPos <= barPos + BAR_HEIGHT + 1 && ballPos >= barPos - 1)
+	if (ballPos <= barPos + BAR_HEIGHT + 1 && ballPos >= barPos - 1) {
 		return 1;
-	else
+	}
+	else {
 		return 0;
+	}
 }
 
 void check_collision(ballData* ball, playerData* player) {
 	// check if a players bar collides with the ball
-	/*
-	if (in_range(ball->yPos, player->posRight))
-		seg_num(1);
-	else
-		seg_num(0);
-	*/
 	if ((int) ball->xPos >= LEFT_COLLISION && in_range(ball->yPos, player->posLeft)) {
 		ball_reflect(ball, 1);
 		ball->speed *= 1.1;
@@ -116,15 +93,15 @@ void check_collision(ballData* ball, playerData* player) {
 		ball->speed *= 1.1;
 		return;
 	}
-	// if not check if one player lost the game by let the ball touch the left or right wall
+	// if not check if one player lost the game by letting the ball touch the left or right wall
 	if (ball->xPos == 0) {
 		player->pointsLeft++;
-		restart(ball);
+		ball_init(ball);
 		return;
 	}
 	if (ball->xPos == GAME_WIDTH - 1) {
 		player->pointsRight++;
-		restart(ball);
+		ball_init(ball);
 		return;
 	}
 	// check if the ball collides with the top or bottom wall
@@ -137,20 +114,23 @@ void check_collision(ballData* ball, playerData* player) {
 void ball_move(ballData* ball) {
 	// calculate new x position
 	ball->xPos += ball->speed * ball->xDir;
-	if (ball->xPos > GAME_WIDTH - 1)
+	if (ball->xPos > GAME_WIDTH - 1) {
 		ball->xPos = GAME_WIDTH - 1;
-	if (ball->xPos < 0)
+	}
+	if (ball->xPos < 0) {
 		ball->xPos = 0;
+	}
 	// calculate new y position
 	ball->yPos += ball->speed * ball->yDir;
-	if (ball->yPos > GAME_HEIGHT - 1)
+	if (ball->yPos > GAME_HEIGHT - 1) {
 		ball->yPos = GAME_HEIGHT - 1;
-	if (ball->yPos < 0)
+	}
+	if (ball->yPos < 0) {
 		ball->yPos = 0;
+	}
 }
 
 int convert(int pos) {
-	//seg_num((ADC_COUNT - 1 - pos) * CONVERT);
 	return (ADC_COUNT - 1 - pos) * CONVERT;
 }
 
@@ -174,9 +154,9 @@ void draw(ballData* ball, playerData* player) {
 	lcd_clear(0);
 	// draw ball;
 	lcd_drawCircle(ball->xPos, ball->yPos, BALL_RADIUS, 1, 1);
-	// draw new right bar
+	// draw right bar
 	draw_bar(0, player->posRight, BAR_WIDTH, BAR_HEIGHT, 1);
-	// draw new right bar
+	// draw new left bar
 	draw_bar(GAME_WIDTH - BAR_WIDTH, player->posLeft, BAR_WIDTH, BAR_HEIGHT, 1);
 }
 
@@ -193,18 +173,21 @@ void main(void) {
 	ball_init(&ball);
 	player_init(&player);
 	for (;;) {
+		// TODO show player points
 		// show points of one player
-		/*
-		if (!playerSeg)
+		/*if (!playerSeg) {
 			seg_num(player.pointsLeft);
-		else
+		}
+		else {
 			seg_num(player.pointsRight);
-			*/
+		}
 		// toggle points view
-		/*if (buttons_get(0))
+		if (buttons_get(0)) {
 			playerSeg = 0;
-		if (buttons_get(1))
-			playerSeg = 1;*/
+		}
+		if (buttons_get(1)) {
+			playerSeg = 1;
+		}*/
 		// move the bars with the A/D Converters
 		bar_move(&player, convert(adc_getValue(2)), convert(adc_getValue(1)));
 		// draw the ball and the bars
@@ -216,5 +199,4 @@ void main(void) {
 		// wait for the next frame
 		delay_ms(GAME_MS_PER_FRAME);
 	}
-
 }
