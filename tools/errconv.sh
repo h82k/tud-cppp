5e3cf51353f2c96e84ac0ac1bd70b4a25ce8ad14
Laurenz Kamp <laurenz.kamp@gmx.de>
2016-04-20 14:31:51 +0200

    Updated to support multiple error messages.

--- tools/errconv.sh
+++ tools/errconv.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-read input;
+while read input
+do
+	if [ "${input:0:3}" == "***" ]
+	then
+		line=$(echo "$input" | grep -oP '\(\K[^\)]+' | head -n1);
+		file=$(echo "$input" | grep -oP '\w*\.[ch]' | head -n1);
+		errmsg=$(echo "$input" | grep -oP ':.*' | head -n1);
+		echo $file:$line:0: error$errmsg;
+		err=1;
+	fi
+done
 
-if [ "${input:0:3}" == "***" ]
+if [ "$err" = "1" ]
 then
-	line=$(echo "$input" | grep -oP '\(\K[^\)]+' | head -n1);
-	file=$(echo "$input" | grep -oP '\w*\.[ch]' | head -n1);
-	errmsg=$(echo "$input" | grep -oP ':.*' | head -n1);
-	echo $file:$line:0: error$errmsg;
 	exit 1;
+else
+	exit 0;
 fi
-
-exit;
