f439bc7e0123c09a0f5c98105e78e991b832f5f3
Philipp Jonczyk <p.jonczyk@posteo.de>
2016-09-08 11:48:40 +0200

    created solution for functor exercise

--- solutions/day4/d4_s05_p04_functional/main.cpp
+++ solutions/day4/d4_s05_p04_functional/main.cpp
@@ -5,44 +5,32 @@
 // for map
 class Square {
 	public:
-		Square(int i):i(i) {}
-		int operator()(){ return i*i; }
-	private:
-		int i;
+		Square() {}
+		int operator() (int &i) { return i*i; }
 };
 
 // for filter
 class Odd {
 	public:
-		Odd(int i):i(i) {}
-		bool operator()() { return i%2 == 1 ? true : false ; } 
-	private:
-		int i;
+		Odd() {}
+		bool operator() (int &i) { return i%2 == 1 ? true : false ; } 
 };
 
 // for reduce
 class Sum {
 	public:
-		Sum(int &i, int j): i(i), j(j) {}
-		int operator()() { return i + j; }
-	private:
-		int i, j;
+		Sum() {}
+		int operator() ( int &i, int j ) { return i + j; }
 };
-
-std::string to_string(std::string &str, int j) { 
-	return str.empty() ? std::to_string(j) : str + " - " + std::to_string(j);
-}
 
 template<typename InIt, typename OutIt>
 OutIt map(InIt first, InIt last, OutIt out_first, Square s) {
-	while(first != last) {
-        *out_first++ = func(*first++);
-    }
+	while(first != last) { *out_first++ = s(*first++); }
 	return out_first;
 }
 
 template<typename ItType, typename OutIt>
-ItType filter(ItType first, ItType last, OutIt out_first, Odd o) {
+ItType filter(ItType first, ItType last, OutIt out_first, Odd pred) {
 	while(first != last) {
         if(pred(*first)) {
 			*out_first++ = *first++;
@@ -61,11 +49,22 @@ T reduce(ItType first, ItType last, T init, Sum s) {
     return init;
 }
 
+void print(const std::vector<int> &numbers) {
+	for (auto i = numbers.begin(); i != numbers.end(); ++i) {
+		std::cout << *i << ", ";
+	}
+	std::cout << std::endl;
+}
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
-
-	std::cout 	<< "Filter:\t[ "
-				<< reduce(numbers.begin(), numbers.end(), std::string {}, to_string)
-				<< " ]" << std::endl;
-
+	map(numbers.begin(), numbers.end(), numbers.begin(), Square()); 
+	filter(numbers.begin(), numbers.end(), numbers.begin(), Odd()); 
+	std::cout << reduce(numbers.begin(), numbers.end(), 0, Sum()) << std::endl;
+	print(numbers);
 }
+
+std::string to_string(std::string &str, int j) { 
+	return str.empty() ? std::to_string(j) : str + " - " + std::to_string(j);
+}
+
